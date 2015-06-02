d1d041b25f529610af105f701926ab7093f19357
paulchino <paulchino@gmail.com>
2015-06-01 18:35:51 -0700

    mongo test

--- models/Drawing.js
+++ models/Drawing.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://heroku_app37421135:na9om96ejff2tlj660otrfiomd@ds043082.mongolab.com:43082/heroku_app37421135');
+// mongoose.connect('mongodb://heroku_app37421135:na9om96ejff2tlj660otrfiomd@ds043082.mongolab.com:43082/heroku_app37421135');
 // mongoose.connect('mongodb://localhost/draw_with_friends');
 
 var DrawingSchema = new mongoose.Schema({
