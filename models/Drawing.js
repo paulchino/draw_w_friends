d568db536077bc328a24f841925592d3d4b9b744
paulchino <paulchino@gmail.com>
2015-06-01 18:22:36 -0700

    mongo labs

--- models/Drawing.js
+++ models/Drawing.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/draw_with_friends');
+
+mongoose.connect('mongodb://heroku_app37421135:na9om96ejff2tlj660otrfiomd@ds043082.mongolab.com:43082/heroku_app37421135');
+// mongoose.connect('mongodb://localhost/draw_with_friends');
 
 var DrawingSchema = new mongoose.Schema({
 	image: String,
